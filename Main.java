8bcf7207459039cb1fe3c5f37d6c7600a908689a
wbsuser <sunwb_mai@163.com>
2020-04-26 22:52:06 +0800

    Main:完全二叉树最短路径和

--- Main.java
+++ Main.java
@@ -11,43 +11,72 @@ import java.util.function.ToIntFunction;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(ReverseSentence(" "));
-    }
-
-
-    public static String ReverseSentence(String str) {
-        if (str == null || str.length() <= 1)
-            return str;
-        String[] strArr = str.split(" ");
-        StringBuilder res = new StringBuilder();
-        for (int i = strArr.length - 1; i >= 0; i--) {
-            res.append(strArr[i] + " ");
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> res = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[][] path = new int[10][10];
+        String[] inStr = new String[n];
+        for (int i = 0; i < n; i++) {
+            inStr[i] = scanner.nextLine();
         }
-        return res.substring(0, res.length()-1);
-    }
-    public static ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
-        int len = (sum + 1) / 2 + 1;
-        int[][] dp = new int[len][len];
-        Arrays.fill(dp[0], 0);
+//        System.out.println(Arrays.toString(inStr));
 
-        for (int i = 0; i < len; i++) {
-            dp[i][0] = 0;
-            dp[i][i] = i;
-        }
-        for (int i = 1; i < len; i++) {
-            for (int j = i + 1; j < len ; j++) {
-                dp[i][j] = dp[i][j-1] + j;
-                if (dp[i][j] == sum) {
-                    ArrayList<Integer> list = new ArrayList<>();
-                    for (int k = i; k <= j; k++)
-                        list.add(k);
-                    res.add(list);
-                }
+        for (String s : inStr) {
+            String[] ss = s.split(" ");
+            int start = Integer.parseInt(ss[1]);
+            int end = Integer.parseInt(ss[2]);
+            if ("1".equals(ss[0])) {
+                int w = Integer.parseInt(ss[3]);
+                update(path, start, end, w);
+            } else {
+                res.add(getCost(path, start, end));
             }
         }
-        return res;
+        for (int r : res)
+            System.out.println(r);
+    }
+
+    private static int getCost(int[][] path, int start, int end) {
+        int cost = 0;
+        int p1, p2;
+        p1 = start;
+        p2 = end;
+        while (p1 != p2) {
+            if (p1 < p2) {
+                int p = p2;
+                p2 = (p2 % 2 == 0) ? p2 / 2 : (p2 - 1) / 2;
+                cost += path[p][p2];
+
+            } else {
+                int p = p1;
+                p1 = (p1 % 2 == 0) ? p1 / 2 : (p1 - 1) / 2;
+                cost += path[p][p1];
+            }
+//            System.out.println("Cost: " + cost);
+        }
+        return cost;
+    }
+
+    private static void update(int[][] path, int start, int end, int w) {
+        int p1, p2;
+        p1 = start;
+        p2 = end;
+        while (p1 != p2) {
+            if (p1 < p2) {
+                int p = p2;
+                p2 = (p2 % 2 == 0) ? p2 / 2 : (p2 - 1) / 2;
+                path[p2][p] += w;
+                path[p][p2] += w;
+            } else {
+                int p = p1;
+                p1 = (p1 % 2 == 0) ? p1 / 2 : (p1 - 1) / 2;
+                path[p1][p] += w;
+                path[p][p1] += w;
+            }
+        }
+//        System.out.println("parent : " + p1);
     }
 
 }
