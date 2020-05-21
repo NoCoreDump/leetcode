20c1a6999d612a74197a8db1744bbe9758fcadc0
wbsuser <37771847+wbsuser@users.noreply.github.com>
2020-05-21 09:04:55 +0800

    quick Sort

--- sort/Sort.java
+++ sort/Sort.java
@@ -17,7 +17,7 @@ public class Sort {
 
     /*
     * @Description 插入排序
-    * @param arr 输入的无需数组
+    * @param arr 无序数组
     * @Return void
     * @Author sunwb
     * @Date 2020/4/11 22:22
@@ -69,7 +69,44 @@ public class Sort {
     }
 
     /*
-    * @Description 交换数组的两个值
+     * @Description 随机快排
+     * @param arr 无序数组
+     * @Return void
+     * @Author sunwb
+     * @Date 2020/4/11 22:22
+     **/
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int L, int R) {
+        if (L < R) {
+            int[] index = partition(arr, L, R);
+            quickSort(arr, L, index[0]);
+            quickSort(arr, index[1], R);
+        }
+    }
+
+    private static int[] partition(int[] arr, int L, int R) {
+        int less = L - 1;
+        int more = R + 1;
+        Random random = new Random();
+        int x = arr[L + random.nextInt(R - L)];
+        while (L < more) {
+            if (arr[L] < x) {
+                swap(arr, ++less, L++);
+            } else if (arr[L] > x) {
+                swap(arr, L, --more);
+            } else {
+                L++;
+            }
+        }
+        return new int[] {less, more};
+    }
+
+    /*
+    * @Description 交换数组的两个值, 位运算写法有风险（两数相等时，有一个为0），需要先判断两个值是否相等
     * @param arr
     * @param i
     * @param j
@@ -78,17 +115,30 @@ public class Sort {
     * @Date 2020/4/27 20:36
     **/
     public static void swap(int[] arr, int i , int j) {
+        if (arr[i] == arr[j]) return;
         //交换数组的两个值，位运算写法
         arr[i] = arr[i] ^ arr[j];
         arr[j] = arr[i] ^ arr[j];
         arr[i] = arr[i] ^ arr[j];
     }
     public static void main(String[] args) {
-        data = GeneratorArray.generateRandomArray(100, 20);
+//        data = GeneratorArray.generateRandomArray(100, 20);
+        for (int i = 0; i < 20; i++) {
+            data = GeneratorArray.generateRandomArray(100, 20);
+            int[] data1 = Arrays.copyOf(data, data.length);
+            quickSort(data);
+            mergeSort(data1);
+            for (int j = 0; j < data.length; ++j) {
+                if (data[j] != data1[j]) {
+                    System.out.println("*******************fuck**********************\n" + Arrays.toString(data) + "\n" + Arrays.toString(data1));
+                    break;
+                }
+            }
+        }
 //        insertSort(data);
 //        System.out.println("insert sort: " + Arrays.toString(data));
-
-        mergeSort(data);
-        System.out.println("merge sort: " + Arrays.toString(data));
+//        quickSort(data);
+//        mergeSort(data);
+//        System.out.println("quick sort: " + Arrays.toString(data));
     }
 }
